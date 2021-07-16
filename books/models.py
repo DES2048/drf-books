6f3fd8d593e4c25676aebdf6fb6afcae0b858dfe
DES2048 <electrochange@mail.ru>
2021-07-16 17:47:49 +0300

    - add str method to Book model, register models in admin

--- books/models.py
+++ books/models.py
@@ -12,3 +12,6 @@ class Book(models.Model):
     title = models.CharField(max_length=256)
 
     authors = models.ManyToManyField(Author, related_name="books")
+
+    def __str__(self):
+        return self.title
